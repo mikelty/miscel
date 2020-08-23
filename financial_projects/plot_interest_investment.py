1da2a373b499b8dbd1b58da163ce73c7fdd04249
mikelty <1autumnpear>
2020-08-23 10:17:57 +0800

    plotter & basic gui working

--- financial_projects/plot_interest_investment.py
+++ financial_projects/plot_interest_investment.py
@@ -3,14 +3,14 @@ from utils import calc_interest
 import matplotlib.pyplot as plt
 from matplotlib.pylab import style
 
-def plot_varying_funds():
+def plot_varying_funds(最低本金,最高本金,还钱总数,周期数):
     style.use('ggplot')
     plt.rcParams['font.sans-serif'] = ['SimHei']
     plt.rcParams['axes.unicode_minus'] = False
 
-    x=np.linspace(25,45,10)
+    x=np.linspace(最低本金,最高本金,10)
     x=[round(x_i,1) for x_i in x]
-    y=[calc_interest(x_i,107,7) for x_i in x]
+    y=[calc_interest(x_i,还钱总数,周期数) for x_i in x]
     plt.plot(x,y)
     plt.xlabel('本金')
     plt.ylabel('周期利率（百分比）')
--- financial_projects/等额本息利率计算器.py
+++ financial_projects/等额本息利率计算器.py
@@ -1,28 +1,54 @@
+#20-40
 import tkinter as tk
 from tkinter import messagebox
 from utils import calc_interest
+from plot_interest_investment import plot_varying_funds
 
 root=tk.Tk()
 root.title('等额本息利率计算器')
 
-var_names=['本金','还钱总数','周期数']
-defaults=[45,107,7]
-data=[0.0 for _ in range(len(var_names))]
-for r,(var,val) in enumerate(zip(var_names,defaults)):
-	label=tk.Label(text=var,width=25)
-	label.grid(row=r,column=0)
-	data[r]=tk.DoubleVar(value=defaults[r])
-	entry=tk.Entry(fg="white",bg="grey",width=50,textvariable=data[r])
-	entry.grid(row=r,column=1)
-
 def on_submit():
-	global 本金,还钱总数,周期数
-	本金 = data[0].get()
-	还钱总数 = data[1].get()
-	周期数 = int(data[2].get())
-	interest=calc_interest(本金,还钱总数,周期数)
-	messagebox.showinfo("周期利率",str(round(interest,2))+'%') #27.9
+	global 本金,还钱总数,周期数,data
+	if sel==CALC_MODE:
+		本金 = data[0].get()
+		还钱总数 = data[1].get()
+		周期数 = int(data[2].get())
+		interest=calc_interest(本金,还钱总数,周期数)
+		messagebox.showinfo("周期利率",str(round(interest,2))+'%') #27.9
+	else:
+		plot_varying_funds(*data)
 
-submit=tk.Button(root,text="提交",command=on_submit)
-submit.grid(row=len(var_names),column=1)
+var_names, defaults = [], []
+sel,CALC_MODE,PLOT_MODE=tk.IntVar(),0,1
+sel.set(CALC_MODE)
+def sel_mode():
+	global data
+	print('a')
+	if sel.get() == CALC_MODE:
+
+		var_names=['本金']
+		defaults=[45]
+	else:
+		var_names=['最低本金','最高本金']
+		defaults=[25,45]
+	var_names=var_names+['还钱总数','周期数']
+	defaults=defaults+[107,7]
+	data=[0.0 for _ in range(len(var_names))]
+	for r,(var,val) in enumerate(zip(var_names,defaults)):
+		label=tk.Label(text=var,width=25)
+		label.grid(row=r+1,column=0)
+		data[r]=tk.DoubleVar(value=defaults[r])
+		entry=tk.Entry(fg="white",bg="grey",width=50,textvariable=data[r])
+		entry.grid(row=r+1,column=1)
+
+	submit=tk.Button(root,text="提交",command=on_submit)
+	submit.grid(row=len(var_names)+1,column=1)
+	print(var_names)
+
+calc_mode=tk.Radiobutton(root,text='计算周期利率（默认）',variable=sel,value=CALC_MODE,command=sel_mode)
+calc_mode.grid(row=0,column=0)
+calc_mode.invoke()
+plot_mode=tk.Radiobutton(root,text='绘制不同本金的周期利率',variable=sel,value=PLOT_MODE,command=sel_mode)
+plot_mode.grid(row=0,column=1)
+
 root.mainloop()
